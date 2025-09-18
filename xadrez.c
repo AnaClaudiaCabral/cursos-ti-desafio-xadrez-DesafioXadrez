a624535775992689b3065cc2ecaeeac95c8d4508
AnaClaudiaCabral <traderclaudynha@gmail.com>
2025-09-18 13:36:48 +0000

    Novo commit

--- xadrez.c
+++ xadrez.c
@@ -4,12 +4,13 @@
 // Ana Claudia Cabral
 // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
-// Nível Novato - Movimentação das Peças
+// Nível Aventureiro - Movimentando o Cavalo
     
     int main() {
     // Simulação de Movimentação da Torre - estrutura de repetição for
     printf("Movimento da Torre: \n");
     for(int i = 1; i <= 5; i ++) {
+    printf("Direita - \t");
     printf("Torre moveu 1 casa para a Direita (total: %d casas)\n",i);
     } 
     printf("\n");
@@ -19,8 +20,9 @@
     int j = 1;
     while (j <= 5)
     {
-        printf("Bispo moveu 1 casa na diagonal (Cima, Direita) (total: %d casas)\n", j);
-        j++;
+    printf("Cima Direira - \t");
+    printf("Bispo moveu 1 casa na diagonal (Cima, Direita) (total: %d casas)\n", j);
+    j++;
     }
     printf("\n");
 
@@ -28,13 +30,29 @@
     printf("Movimento da Rainha: \n");
     int k = 1;
     do {
-        printf("Rainha moveu 1 casa para a Esquerda (total: %d casas)\n", k);
-        k++;
+    printf("Esquerda - \t");
+    printf("Rainha moveu 1 casa para a Esquerda (total: %d casas)\n", k);
+    k++;
     } while (k <= 8);
     printf("\n");
-    printf("Novo Commit \n");
-printf("novo commit \n");
-           
 
-    return 0;
+    //Simulação de Movimento do Cavalo - usando for e while aninhados
+    printf("Movimento do Cavalo: \n");
+    int casas_verticais;
+    for (casas_verticais = 1; casas_verticais <= 2; casas_verticais++){
+    printf("Cima - \t");
+    printf("Cavalo moveu 2 casas para cima (total: %d casas)\n", casas_verticais);
+    }
+
+    int casas_horizontais = 1;
+    while (casas_horizontais <= 1){
+    printf("Direita - \t");
+    printf("Cavalo moveu 1 casa para a Direita (total: %d casas)\n", casas_horizontais);
+    casas_horizontais++;
+    }
+    printf("\n");
+
+    
+
+return 0;
 }
